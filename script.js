0d7a4be59cc8c748a65f701bfe3a1b3bcaaf73e9
Prateek Khindri <prateek.khindri90@gmail.com>
2022-04-28 11:08:14 +1000

    Created an array of objects and stored each product

---
+++ script.js
@@ -0,0 +1,60 @@
+// 1. We create an array of objects and display them onscreen
+
+const productArray = [
+  {
+    productID: 1,
+    productImg:
+      "https://images.unsplash.com/photo-1561808843-7adeb9606939?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzR8fHNob2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    productName: "Breezy My Heart",
+    unitPrice: "300",
+  },
+  {
+    productID: 2,
+    productImg:
+      "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    productName: "Megatron The Ruler",
+    unitPrice: "1200",
+  },
+  {
+    productID: 3,
+    productImg:
+      "https://images.unsplash.com/photo-1583979365152-173a8f14181b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTU3fHxzaG9lc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    productName: "Bumblebee Forever",
+    unitPrice: "800",
+  },
+  {
+    productID: 4,
+    productImg:
+      "https://images.unsplash.com/photo-1539185441755-769473a23570?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjR8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    productName: "How about Jazz",
+    unitPrice: "600",
+  },
+  {
+    productID: 5,
+    productImg:
+      "https://images.unsplash.com/photo-1508609349937-5ec4ae374ebf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTU1fHxzaG9lc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    productName: "Optimus Prime",
+    unitPrice: "1500",
+  },
+  {
+    productID: 6,
+    productImg:
+      "https://images.unsplash.com/photo-1600185365778-7875a359b924?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTh8fHNob2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    productName: "Mud Flap Vow",
+    unitPrice: "500",
+  },
+  {
+    productID: 7,
+    productImg:
+      "https://images.unsplash.com/photo-1520256862855-398228c41684?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTQ2fHxzaG9lc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    productName: "Hound The Sky",
+    unitPrice: "600",
+  },
+  {
+    productID: 8,
+    productImg:
+      "https://media.istockphoto.com/photos/fashion-white-sneakers-in-neon-light-sport-shoes-for-training-in-the-picture-id1301394040?b=1&k=20&m=1301394040&s=170667a&w=0&h=DM2FyLVbwZ-YjZeb5P8d45RgF2x_gTmT8kC66u7zIrk=",
+    productName: "Drift Rule All",
+    unitPrice: "2000",
+  },
+];
